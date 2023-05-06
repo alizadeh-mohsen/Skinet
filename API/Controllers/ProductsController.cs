b74d6cbcfa24bf780332373bbd44ff7a26e27ff2
Mohsen.Alizadeh <Mohsen.Alizadeh@TALANUK1LL2CL3>
2023-05-06 23:02:01 +0100

    Add project files.

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "All Products listed!";
+        }
+    }
+}
