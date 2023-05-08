3ea8d9a5f2608079bd978e317115ea1dcbd346a7
Mohsen Alizadeh <Mohsen.Alizadeh@talan.com>
2023-05-08 01:09:48 +0100

    client

--- API/ExtensionMethods/ServiceCollectionExtension.cs
+++ API/ExtensionMethods/ServiceCollectionExtension.cs
@@ -19,7 +19,7 @@ namespace API.ExtensionMethods
             {
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
                 });
             });
             return services;
